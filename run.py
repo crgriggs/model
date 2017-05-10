91e54423dfdad7280ed6e78b0caefd17bfa88de7
Chris Griggs <cgriggs@cs.unc.edu>
2017-05-10 15:26:00 -0400

    y uclid

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import os
 
-os.system("python psuedo.py psuedocode/mov.txt > models/mov.txt")
-os.system("python psuedo.py psuedocode/push.txt > models/push.txt")
-os.system("python psuedo.py psuedocode/pop.txt > models/pop.txt")
-os.system("python psuedo.py psuedocode/sysret.txt > models/sysret.txt")
-os.system("python psuedo.py psuedocode/syscall.txt > models/syscall.txt")
-os.system("python createState.py models/")
+# os.system("python psuedo.py psuedocode/mov.txt > models/mov.txt")
+# os.system("python psuedo.py psuedocode/push.txt > models/push.txt")
+# os.system("python psuedo.py psuedocode/pop.txt > models/pop.txt")
+# os.system("python psuedo.py psuedocode/sysret.txt > models/sysret.txt")
+# os.system("python psuedo.py psuedocode/syscall.txt > models/syscall.txt")
+# os.system("python createState.py models/")
 os.system("/home/rhett/Documents/uclid/bin/uclid state.ucl > counterExample.txt")
 os.system("python readCounterExample.py")
