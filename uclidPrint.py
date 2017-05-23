9b58847fb45f92382d9fd8e1d3f4ed11b00dceb2
Chris Griggs <cgriggs@cs.unc.edu>
2017-05-23 14:53:13 -0400

    everythings there

--- uclidPrint.py
+++ uclidPrint.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import re
 
-stateVars = {'memory' : '1', 'rsp': '64','r11': '64', 'CR0' : '64', 'CS': '64', 'SS': '64', 'cs_selector': '16', 'cs_base': '32', 'cs_limit': '20', 'cs_accessRights': '12', 'cpl': '2', 'ss_selector': '16', 'ss_base': '32', 'ss_limit': '20', 'ss_accessRights': '12', 'EFER': '64', 'CR4': '64', 'rflags': '64', 'rcx': '64', 'rip': '64'}
+stateVars = {'rdx': '64','memory' : '1', 'rsp': '64','r11': '64', 'CR0' : '64', 'CS': '64', 'SS': '64', 'cs_selector': '16', 'cs_base': '32', 'cs_limit': '20', 'cs_accessRights': '12', 'cpl': '2', 'ss_selector': '16', 'ss_base': '32', 'ss_limit': '20', 'ss_accessRights': '12', 'EFER': '64', 'CR4': '64', 'rflags': '64', 'rcx': '64', 'rip': '64'}
 rflagsDict ={'CF': 'State.rflags # [0:0]', 'PF': 'State.rflags # [1:1]', 'AF': 'State.rflags # [4:4]', 'ZF': 'State.rflags # [6:6]', 'SF': 'State.rflags # [7:7]', 'TF': 'State.rflags # [8:8]', 'IF': 'State.rflags # [9:9]', 'DF': 'State.rflags # [10:10]', 'OF': 'State.rflags # [11:11]', 'IOPL': 'State.rflags # [13:12]', 'NT': 'State.rflags # [14:14]', 'RF': 'State.rflags # [16:16]', 'VM': 'State.rflags # [17:17]', 'AC': 'State.rflags # [18:18]', 'VIF': 'State.rflags # [19:19]', 'VIP': 'State.rflags # [20:20]', 'ID': 'State.rflags # [21:21]'}
 CR0Dict = {'PE': 'State.CR0 # [0:0]', 'MP': 'State.CR0 # [1:1]', 'EM': 'State.CR0 # [2:2]', 'TS': 'State.CR0 # [3:3]', 'ET': 'State.CR0 # [4:4]', 'NE': 'State.CR0 # [5:5]', 'WP': 'State.CR0 # [16:16]', 'AM': 'State.CR0 # [18:18]','NW': 'State.CR0 # [29:29]', 'CD': 'State.CR0 # [30:30]', 'PG': 'State.CR0 # [31:31]'}
 CR4Dict = {'VME': 'State.CR4 # [0:0]', 'PVI': 'State.CR4 # [1:1]', 'TSD': 'State.CR4 # [2:2]', 'DE': 'State.CR4 # [3:3]', 'PSE': 'State.CR4 # [4:4]', 'PAE': 'State.CR4 # [5:5]', 'MCE': 'State.CR4 # [6:6]', 'PGE': 'State.CR4 # [7:7]','PCE': 'State.CR4 # [8:8]', 'OSFXSR': 'State.CR4 # [9:9]', 'OSXMMEXCPT': 'State.CR4 # [10:10]', 'VMXE': 'State.CR4 # [13:13]', 'SMXE': 'State.CR4 # [14:14]', 'FSGSBASE': 'State.CR4 # [16:16]', 'PCIDE': 'State.CR4 # [17:17]', 'OSXSAVE': 'State.CR4 # [18:18]', 'SMEP': 'State.CR4 # [20:20]', 'SMAP': 'State.CR4 # [21:21]', 'PKE': 'State.CR4 # [22:22]'}
@@ -193,8 +193,13 @@ ucl_hex_f := ( b1 @ b1 @ b1 @ b1);
             line = line.replace("+", (" "))
             line = line.replace("[]", "")
             line = line.replace("DEST", "")
+            line = line.replace("(", "")
+            line = line.replace(")", "")
+            line = line.replace("@", "")
             if "#" in line:
                 line = line.split("#")[0]
+            if "concat" in line:
+                line = "(" + line.replace("concat", ") @ (") + ")"
             for word in line.split():
                 word = word.strip()
                 if word == "if":
@@ -248,8 +253,10 @@ ucl_hex_f := ( b1 @ b1 @ b1 @ b1);
                     self.defines.add(word + " := State.rsp # [15:0];" )
                 elif len(word) == 3 and "X" == word[2]:
                     self.defines.add(word + " := State.r" + word[1].lower() + "x # [31:0];" )
+                    self.inputs.add("r" + word[1].lower() + "x" )
                 elif len(word) == 2 and "X" == word[1]:
                     self.defines.add(word + " := State.r" + word[1].lower() + "x # [15:0];" )
+                    self.inputs.add("r" + word[1].lower() + "x" )
                 elif word in ['StackAddrSize', 'OperandSize']:
                     self.cvd[word] = [['{bits64, bits32, bits16}'], None, None]
                 elif word == "isCanonical":
@@ -257,6 +264,8 @@ ucl_hex_f := ( b1 @ b1 @ b1 @ b1);
                 elif word == "SRC":
                     self.cvd[word] = [['RNG(SRC)', None, None]]
                     self.constants.add("RNG")
+                elif "MSR" in word:
+                    self.inputs.add("manyCacheTypes")
                 else:
                     self.constants.add(word)
 
@@ -269,6 +278,8 @@ ucl_hex_f := ( b1 @ b1 @ b1 @ b1);
                 print "State." + str(key) + " : FUNC[" + str(stateVars[key]) +"];"
             elif key == "isCanonical":
                 print key + " : TRUTH;"
+            elif key == "manyCacheTypes":
+                print "State." + str(key) + " : FUNC[1] of cacheType;"
             else:
                 print "State." + str(key) + " : BITVEC[" + str(stateVars[key]) +"];"
 
@@ -443,7 +454,9 @@ ucl_hex_f := ( b1 @ b1 @ b1 @ b1);
                     rflags = False
                     print "rflags : BITVEC[64];"
             elif "SRC" in var.strip():
-                print "SRC : BITVEC[64];" 
+                print "SRC : BITVEC[64];"
+            elif "MSR" in var:
+                print "temp : BITVEC[64];"
             else:
                 if var not in stateVars:
                     var = var.lower()
